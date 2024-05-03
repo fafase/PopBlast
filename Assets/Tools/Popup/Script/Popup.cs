3566eab969c1fc37b27289bf3101f619f9afe171
fafase <lucascoss@gmail.com>
2024-05-03 14:04:51 +0300

    Popup - Open Close animations

--- Assets/Tools/Popup/Script/Popup.cs
+++ Assets/Tools/Popup/Script/Popup.cs
@@ -1,35 +1,95 @@
-using Codice.Client.BaseCommands.WkStatus.Printers;
+using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.UI;
+using State = Tools.IPopup.State;
 
 namespace Tools
 {
+    [RequireComponent(typeof(Animation))]
     public abstract class Popup : MonoBehaviour, IPopup
     {
         [SerializeField] private Button m_closeBtn;
+        [SerializeField] private AnimationClip m_openAnimation;
+        [SerializeField] private AnimationClip m_closeAnimation;
 
         private IPopupManager m_popupManager;
+        private Animation m_animation;
+        private State m_state = State.Idle;
+        public State PopupState => m_state;
+
+        public event Action<IPopup> OnClose;
+        public event Action <IPopup> OnOpen;
+        
         public void Init(IPopupManager popupManager)
         {
             m_popupManager = popupManager;
             transform.SetParent(m_popupManager.Container, false);
-            m_closeBtn.onClick.AddListener(Close);
+            m_closeBtn.onClick.AddListener(() =>  Close());
+
+            m_animation = GetComponent<Animation>();
+            m_animation.wrapMode = WrapMode.Once;
+            m_animation.AddClip(m_openAnimation, m_openAnimation.name);
+            m_animation.AddClip(m_closeAnimation, m_closeAnimation.name);
+
+            m_state = State.Idle;
+            StartCoroutine(OpenSequence());
         }
 
-        public void Close() 
+        private IEnumerator OpenSequence() 
         {
-            m_popupManager.Close(this);
-            m_closeBtn.onClick.RemoveAllListeners();
+            yield return StartCoroutine(AnimationSequence(State.Opening));
+            SetButtons(true);
+            OnOpen?.Invoke(this);
+        }
+        private IEnumerator CloseSequence() 
+        {
+            yield return StartCoroutine(AnimationSequence(State.Closing));
+            OnClose?.Invoke(this);
             Destroy(gameObject);
         }
+
+
+        private IEnumerator AnimationSequence(State state) 
+        {
+            m_state = state;
+            SetButtons(false);
+            AnimationClip currentAnim = state == State.Opening ? m_openAnimation : m_closeAnimation;
+            m_animation.clip = currentAnim;
+            m_animation.Play();
+            while (m_animation.IsPlaying(currentAnim.name))
+            {
+                yield return null;
+            }
+            m_state = State.Idle;
+        }
+
+        public void Close(bool closeImmediate = false) 
+        {
+            m_popupManager.Close(this);
+            if (closeImmediate) 
+            {
+                Destroy(gameObject);
+                return;
+            }
+            StartCoroutine(CloseSequence());
+        }
+
+        public void AddToClose(Action<IPopup> action) => OnClose += action;
+        public void RemoveToClose(Action<IPopup> action) => OnClose -= action;
+
+        private void SetButtons(bool value) => Array.ForEach(GetComponentsInChildren<Button>(), (btn => btn.enabled = value));
     }
 
     public interface IPopup 
     {
-        void Close();
+        void Close(bool closeImmediate = false);
         void Init(IPopupManager popupManager);
+        void AddToClose(Action<IPopup> action);
+        void RemoveToClose(Action<IPopup> action);
+        public enum State
+        {
+            Idle, Opening, Closing
+        }
     }
 }
