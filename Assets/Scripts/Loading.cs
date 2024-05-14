6519395567843047b7421f09f41101a45d2006ea
fafase <lucascoss@gmail.com>
2024-05-14 18:27:05 +0300

    Added LoadingPopup to loading process

--- Assets/Scripts/Loading.cs
+++ Assets/Scripts/Loading.cs
@@ -1,12 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Tools;
-using UnityEngine.SceneManagement;
 using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tools;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Zenject;
 
 public class Loading : MonoBehaviour
 {
+    [Inject] IPopupManager m_popupManager;
     public void OnInitStart()
     {
         Debug.Log("Start init");
@@ -15,6 +17,21 @@ public class Loading : MonoBehaviour
     public void OnInitComplete(List<InitializationResult> results)
     {
         Debug.Log("End init");
-        SceneManager.LoadSceneAsync("Meta");
+        LoadAsync().Forget();
+    }
+
+    private async UniTask LoadAsync() 
+    {
+        IPopup popup = m_popupManager.Show<LoadingPopup>();
+        while (popup.PopupState != IPopup.State.Idle) 
+        {
+            await Task.Yield();
+        }
+        AsyncOperation loading = SceneManager.LoadSceneAsync("Meta");
+        while (!loading.isDone)
+        {
+            await Task.Yield();
+        }
+        popup.Close();
     }
 }
