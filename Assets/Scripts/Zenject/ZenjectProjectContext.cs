b9418dc7b5486df554e9eee8512c34b98a29fccd
fafase <lucascoss@gmail.com>
2024-05-14 23:27:58 +0300

    Remove older localization

--- Assets/Scripts/Zenject/ZenjectProjectContext.cs
+++ Assets/Scripts/Zenject/ZenjectProjectContext.cs
@@ -5,7 +5,6 @@ using Zenject;
 public class ZenjectProjectContext : MonoInstaller
 {
     [SerializeField] private GameObject m_popupManager;
-    [SerializeField] private Localization m_localization;
 
     public override void InstallBindings()
     {
@@ -13,7 +12,6 @@ public class ZenjectProjectContext : MonoInstaller
         Container.BindInterfacesTo<PopupManager>().FromComponentInNewPrefab(m_popupManager).AsSingle().NonLazy();
         Container.BindInterfacesTo<UserPrefs>().FromNew().AsSingle().NonLazy();
         Container.BindInterfacesTo<ServicesManager>().FromNew().AsSingle().NonLazy();
-        Container.BindInterfacesTo<Localization>().FromNewScriptableObject(m_localization).AsSingle().NonLazy();
         Container.BindInterfacesTo<LevelManager>().FromNew().AsSingle().NonLazy();
         Container.BindInterfacesTo<PlayerData>().FromNew().AsSingle().NonLazy();
 
